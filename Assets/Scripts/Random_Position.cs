f4e4db8539be0f134c5a2db2668dc285b9dce91c
olitillinfintye <oliyadtesfaye2020@gmail.com>
2024-07-10 11:07:23 +0300

    first phase

---
+++ Assets/Scripts/Random_Position.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Random_Position : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // public Vector2 pos;
+    // public int score;
+    // public Text scoreText;
+
+
+    // void Start()
+    // {
+       
+    //     move();
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if(Input.GetMouseButtonDown(0)){
+    //         move();
+            
+    //     }
+    // }
+
+    // void move(){
+    //      transform.position = new Vector2(Random.Range(0,pos.x),Random.Range(0,pos.y));
+
+    // }
+
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+        // score++;
+        // scoreText.text = score.ToString();
+    }
+}
---
+++ Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnerController : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] Targets;
+    private int randomSpawnPoints, randomTarget;
+    public int score;
+    public Text scoreText;
+    private int spawnedTargets = 0;
+    private const int maxTargets = 5;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnATarget", 0f, 1f);
+    }
+
+    private void SpawnATarget()
+    {
+        if (spawnedTargets < maxTargets)
+        {
+            randomSpawnPoints = Random.Range(0, spawnPoints.Length);
+            randomTarget = Random.Range(0, Targets.Length);
+            Instantiate(Targets[randomTarget], spawnPoints[randomSpawnPoints].position, Quaternion.identity);
+            spawnedTargets++;
+        }
+    }
+
+    private void Update()
+    {
+        // UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
